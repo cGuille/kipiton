33b5e9f640d499fa43794aeae4c73a2863358123
Guillaume Charmetant <cguille.dev@gmail.com>
2016-08-20 22:06:14 +0200

    replace debug with reload

--- app/model/Game.js
+++ app/model/Game.js
@@ -5,17 +5,20 @@
         constructor(canvas) {
             this.canvas = canvas;
             init.call(this);
-
-            this.shipInBoard = null;
-            this.debug = document.getElementById('debug');
         }
 
         start() {
+            this.isStarted = true;
             run.call(this);
+        }
+
+        stop() {
+            this.isStarted = false;
         }
     }
 
     function init() {
+        this.isStarted = false;
         this.board = new Board(this.canvas.width, this.canvas.height, 20);
         this.ship = new Ship({ x: this.board.width / 2, y: this.board.height / 2 });
 
@@ -32,18 +35,19 @@
     }
 
     function run() {
+        if (!this.isStarted) {
+            return;
+        }
+
         this.ship.dx = this.controller.updateDx(this.ship.dx);
         this.ship.dy = this.controller.updateDy(this.ship.dy);
         this.ship.move();
 
-        if (this.shipInBoard !== this.board.contains(this.ship)) {
-            this.shipInBoard = !this.shipInBoard;
-            this.debug.textContent = this.shipInBoard ? 'yes' : 'nope';
+        if (!this.board.contains(this.ship)) {
+            console.log("Game Over!");
+            this.stop();
+            setTimeout(document.location.reload.bind(document.location), 500);
         }
-
-        // if (!this.board.contains(this.ship)) {
-        //     console.log("Game Over!");
-        // }
 
         this.renderer.render();
         requestAnimationFrame(run.bind(this));
