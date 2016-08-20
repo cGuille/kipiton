cfcba79cf89125f88e0c644d5ec353ad3383522c
Guillaume Charmetant <cguille.dev@gmail.com>
2016-08-20 22:55:02 +0200

    improve board drawer
    Make it draw the active danger zones after the inactive ones so that
    they are drawn on top and override them.

--- app/drawer/BoardDrawer.js
+++ app/drawer/BoardDrawer.js
@@ -6,18 +6,54 @@
 
     class BoardDrawer extends Drawer {
         _draw(board) {
-            this.context.fillStyle = board.dangerTop ? ACTIVE_DANGER_ZONE_COLOR : INACTIVE_DANGER_ZONE_COLOR;
-            this.context.fillRect(0, 0, board.width, board.dangerZoneThickness);
+            // Draw inactive danger zones
+            if (!board.dangerTop) {
+                drawTopDangerZone.call(this, board);
+            }
+            if (!board.dangerRight) {
+                drawRightDangerZone.call(this, board);
+            }
+            if (!board.dangerBottom) {
+                drawBottomDangerZone.call(this, board);
+            }
+            if (!board.dangerLeft) {
+                drawLeftDangerZone.call(this, board);
+            }
 
-            this.context.fillStyle = board.dangerRight ? ACTIVE_DANGER_ZONE_COLOR : INACTIVE_DANGER_ZONE_COLOR;
-            this.context.fillRect(board.width - board.dangerZoneThickness, 0, board.dangerZoneThickness, board.height);
-
-            this.context.fillStyle = board.dangerBottom ? ACTIVE_DANGER_ZONE_COLOR : INACTIVE_DANGER_ZONE_COLOR;
-            this.context.fillRect(0, board.height - board.dangerZoneThickness, board.width, board.dangerZoneThickness);
-
-            this.context.fillStyle = board.dangerLeft ? ACTIVE_DANGER_ZONE_COLOR : INACTIVE_DANGER_ZONE_COLOR;
-            this.context.fillRect(0, 0, board.dangerZoneThickness, board.height);
+            // Draw active danger zones
+            if (board.dangerTop) {
+                drawTopDangerZone.call(this, board);
+            }
+            if (board.dangerRight) {
+                drawRightDangerZone.call(this, board);
+            }
+            if (board.dangerBottom) {
+                drawBottomDangerZone.call(this, board);
+            }
+            if (board.dangerLeft) {
+                drawLeftDangerZone.call(this, board);
+            }
         }
+    }
+
+    function drawTopDangerZone(board) {
+        this.context.fillStyle = board.dangerTop ? ACTIVE_DANGER_ZONE_COLOR : INACTIVE_DANGER_ZONE_COLOR;
+        this.context.fillRect(0, 0, board.width, board.dangerZoneThickness);
+    }
+
+    function drawRightDangerZone(board) {
+        this.context.fillStyle = board.dangerRight ? ACTIVE_DANGER_ZONE_COLOR : INACTIVE_DANGER_ZONE_COLOR;
+        this.context.fillRect(board.width - board.dangerZoneThickness, 0, board.dangerZoneThickness, board.height);
+    }
+
+    function drawBottomDangerZone(board) {
+        this.context.fillStyle = board.dangerBottom ? ACTIVE_DANGER_ZONE_COLOR : INACTIVE_DANGER_ZONE_COLOR;
+        this.context.fillRect(0, board.height - board.dangerZoneThickness, board.width, board.dangerZoneThickness);
+    }
+
+    function drawLeftDangerZone(board) {
+        this.context.fillStyle = board.dangerLeft ? ACTIVE_DANGER_ZONE_COLOR : INACTIVE_DANGER_ZONE_COLOR;
+        this.context.fillRect(0, 0, board.dangerZoneThickness, board.height);
     }
 
     window.BoardDrawer = BoardDrawer;
