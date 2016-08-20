7ef02b358c0ebb18b5b2ebc33f0aab14574dab66
Guillaume Charmetant <cguille.dev@gmail.com>
2016-08-20 20:19:54 +0200

    display if ship is in board or not

--- app/model/Board.js
+++ app/model/Board.js
@@ -7,6 +7,20 @@
             this.height = height;
             this.dangerZoneThickness = 20;
         }
+
+        contains(ship) {
+            const shipTop = ship.y - ship.radius;
+            const shipBottom = ship.y + ship.radius;
+            const shipLeft = ship.x - ship.radius;
+            const shipRight = ship.x + ship.radius;
+
+            return (
+                shipTop < this.height &&
+                shipBottom > 0 &&
+                shipLeft < this.width &&
+                shipRight > 0
+            );
+        }
     }
 
     window.Board = Board;
--- app/model/Game.js
+++ app/model/Game.js
@@ -5,6 +5,9 @@
         constructor(canvas) {
             this.canvas = canvas;
             init.call(this);
+
+            this.shipInBoard = null;
+            this.debug = document.getElementById('debug');
         }
 
         start() {
@@ -13,10 +16,16 @@
     }
 
     function init() {
-        this.ship = new Ship({ x: 50, y: 50 });
+        this.ship = new Ship({ x: 20, y: 20 });
+        this.board = new Board(this.canvas.width, this.canvas.height, 20);
 
-        this.renderer = new Renderer(this.canvas, new BoardDrawer(this.canvas), new ShipDrawer(this.canvas));
-        this.renderer.addBoard(new Board(this.canvas.width, this.canvas.height, 20));
+        this.renderer = new Renderer(
+            this.canvas,
+            new BoardDrawer(this.canvas),
+            new ShipDrawer(this.canvas)
+        );
+
+        this.renderer.addBoard(this.board);
         this.renderer.addShip(this.ship);
 
         this.controller = new KeyboardController();
@@ -26,6 +35,15 @@
         this.ship.dx = this.controller.updateDx(this.ship.dx);
         this.ship.dy = this.controller.updateDy(this.ship.dy);
         this.ship.move();
+
+        if (this.shipInBoard !== this.board.contains(this.ship)) {
+            this.shipInBoard = !this.shipInBoard;
+            this.debug.textContent = this.shipInBoard ? 'yes' : 'nope';
+        }
+
+        // if (!this.board.contains(this.ship)) {
+        //     console.log("Game Over!");
+        // }
 
         this.renderer.render();
         requestAnimationFrame(run.bind(this));
